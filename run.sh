595c4c28c62656332ddd05db6bd9ff1dea1f6303
Li YI <control359@gmail.com>
2022-09-08 09:14:05 -0400

    Update run.sh
    update arguments

--- run.sh
+++ run.sh
@@ -3,25 +3,25 @@
 ################
 
 ### noise ratio = 0%
-python -u main.py --data_root /data --dataset cifar10 --lr 0.02 --lamb 50 --ctr_thred 0.8 --r 0 --noise_type sym
+python -u main.py --data_root /data --dataset cifar10 --lr 0.02 --lamb 50 -- tau 0.8 --r 0 --noise_type sym
 
 ### sym noise ratio = 20%
-python -u main.py --data_root /data --dataset cifar10  --lr 0.02 --lamb 50 --ctr_thred 0.4 --r 0.2 --noise_type sym
+python -u main.py --data_root /data --dataset cifar10  --lr 0.02 --lamb 50 -- tau 0.4 --r 0.2 --noise_type sym
 
 ### sym noise ratio = 40%
-python -u main.py --data_root /data --dataset cifar10  --lr 0.02 --lamb 90 --ctr_thred 0.4 --r 0.4 --noise_type sym
+python -u main.py --data_root /data --dataset cifar10  --lr 0.02 --lamb 90 -- tau 0.4 --r 0.4 --noise_type sym
 
 ### sym noise ratio = 60%
-python -u main.py --data_root /data --dataset cifar10  --lr 0.02 --lamb 170 --ctr_thred 0.8 --r 0.6 --noise_type sym
+python -u main.py --data_root /data --dataset cifar10  --lr 0.02 --lamb 170 -- tau 0.8 --r 0.6 --noise_type sym
 
 ### sym noise ratio = 80%
-python -u main.py --data_root /data --dataset cifar10  --lr 0.02 --lamb 130 --ctr_thred 0.4 --r 0.8 --noise_type sym
+python -u main.py --data_root /data --dataset cifar10  --lr 0.02 --lamb 130 -- tau 0.4 --r 0.8 --noise_type sym
 
 ### sym noise ratio = 90%
-python -u main.py --data_root /data --dataset cifar10  --lr 0.02 --lamb 170 --ctr_thred 0.4 --r 0.9 --noise_type sym
+python -u main.py --data_root /data --dataset cifar10  --lr 0.02 --lamb 170 -- tau 0.4 --r 0.9 --noise_type sym
 
 ### asym noise ratio = 40%
-python -u main.py --data_root /data --dataset cifar10  --lr 0.04 --lamb 50 --ctr_thred 0.4 --r 0.4 --noise_type asym
+python -u main.py --data_root /data --dataset cifar10  --lr 0.04 --lamb 50 -- tau 0.4 --r 0.4 --noise_type asym
 
 
 
@@ -30,22 +30,22 @@ python -u main.py --data_root /data --dataset cifar10  --lr 0.04 --lamb 50 --ctr
 ################
 
 ### noise ratio = 0%
-python -u main.py --data_root /data --dataset cifar100 --lr 0.02 --lamb 50 --ctr_thred 0.05 --r 0 --noise_type sym
+python -u main.py --data_root /data --dataset cifar100 --lr 0.02 --lamb 50 -- tau 0.05 --r 0 --noise_type sym
 
 ### sym noise ratio = 20%
-python -u main.py --data_root /data --dataset cifar100  --lr 0.02 --lamb 130 --ctr_thred 0.05 --r 0.2 --noise_type sym
+python -u main.py --data_root /data --dataset cifar100  --lr 0.02 --lamb 130 -- tau 0.05 --r 0.2 --noise_type sym
 
 ### sym noise ratio = 40%
-python -u main.py --data_root /data --dataset cifar100  --lr 0.02 --lamb 130 --ctr_thred 0.05 --r 0.4 --noise_type sym
+python -u main.py --data_root /data --dataset cifar100  --lr 0.02 --lamb 130 -- tau 0.05 --r 0.4 --noise_type sym
 
 ### sym noise ratio = 60%
-python -u main.py --data_root /data --dataset cifar100  --lr 0.02 --lamb 130 --ctr_thred 0.05 --r 0.6 --noise_type sym
+python -u main.py --data_root /data --dataset cifar100  --lr 0.02 --lamb 130 -- tau 0.05 --r 0.6 --noise_type sym
 
 ### sym noise ratio = 80%
-python -u main.py --data_root /data --dataset cifar100  --lr 0.02 --lamb 130 --ctr_thred 0.05 --r 0.8 --noise_type sym
+python -u main.py --data_root /data --dataset cifar100  --lr 0.02 --lamb 130 -- tau 0.05 --r 0.8 --noise_type sym
 
 ### asym noise ratio = 40%
-python -u main.py --data_root /data --dataset cifar100  --lr 0.04 --lamb 170 --ctr_thred 0.05 --r 0.4 --noise_type asym
+python -u main.py --data_root /data --dataset cifar100  --lr 0.04 --lamb 170 -- tau 0.05 --r 0.4 --noise_type asym
 
 
 ################
@@ -53,19 +53,19 @@ python -u main.py --data_root /data --dataset cifar100  --lr 0.04 --lamb 170 --c
 ################
 
 ### sym noise ratio = 20%
-python -u main.py --data_root /data --dataset cifar10  --lr 0.02 --lamb 4.0 --ctr_thred 0.4 --r 0.2 --noise_type sym --type gce --beta 0.6
+python -u main.py --data_root /data --dataset cifar10  --lr 0.02 --lamb 4.0 -- tau 0.4 --r 0.2 --noise_type sym --type gce --beta 0.6
 
 ### sym noise ratio = 40%
-python -u main.py --data_root /data --dataset cifar10  --lr 0.02 --lamb 8.0 --ctr_thred 0.4 --r 0.4 --noise_type sym --type gce --beta 0.6
+python -u main.py --data_root /data --dataset cifar10  --lr 0.02 --lamb 8.0 -- tau 0.4 --r 0.4 --noise_type sym --type gce --beta 0.6
 
 ### sym noise ratio = 60%
-python -u main.py --data_root /data --dataset cifar10  --lr 0.02 --lamb 4.0 --ctr_thred 0.8 --r 0.6 --noise_type sym --type gce --beta 0.8
+python -u main.py --data_root /data --dataset cifar10  --lr 0.02 --lamb 4.0 -- tau 0.8 --r 0.6 --noise_type sym --type gce --beta 0.8
 
 ### sym noise ratio = 80%
-python -u main.py --data_root /data --dataset cifar10  --lr 0.02 --lamb 8.0 --ctr_thred 0.8 --r 0.8 --noise_type sym --type gce --beta 0.8
+python -u main.py --data_root /data --dataset cifar10  --lr 0.02 --lamb 8.0 -- tau 0.8 --r 0.8 --noise_type sym --type gce --beta 0.8
 
 ### sym noise ratio = 90%
-python -u main.py --data_root /data --dataset cifar10  --lr 0.02 --lamb 8.0 --ctr_thred 0.4 --r 0.9 --noise_type sym --type gce --beta 0.8
+python -u main.py --data_root /data --dataset cifar10  --lr 0.02 --lamb 8.0 -- tau 0.4 --r 0.9 --noise_type sym --type gce --beta 0.8
 
 
 ################
@@ -73,16 +73,16 @@ python -u main.py --data_root /data --dataset cifar10  --lr 0.02 --lamb 8.0 --ct
 ################
 
 ### noise ratio = 0%
-python -u main.py --data_root /data --dataset cifar100 --lr 0.02 --lamb 8.0 --ctr_thred 0.05 --r 0 --noise_type sym --type gce --beta 0.6
+python -u main.py --data_root /data --dataset cifar100 --lr 0.02 --lamb 8.0 -- tau 0.05 --r 0 --noise_type sym --type gce --beta 0.6
 
 ### sym noise ratio = 20%
-python -u main.py --data_root /data --dataset cifar100  --lr 0.02 --lamb 8.0 --ctr_thred 0.05 --r 0.2 --noise_type sym --type gce --beta 0.6
+python -u main.py --data_root /data --dataset cifar100  --lr 0.02 --lamb 8.0 -- tau 0.05 --r 0.2 --noise_type sym --type gce --beta 0.6
 
 ### sym noise ratio = 40%
-python -u main.py --data_root /data --dataset cifar100  --lr 0.02 --lamb 8.0 --ctr_thred 0.05 --r 0.4 --noise_type sym --type gce --beta 0.6
+python -u main.py --data_root /data --dataset cifar100  --lr 0.02 --lamb 8.0 -- tau 0.05 --r 0.4 --noise_type sym --type gce --beta 0.6
 
 ### sym noise ratio = 60%
-python -u main.py --data_root /data --dataset cifar100  --lr 0.02 --lamb 8.0 --ctr_thred 0.05 --r 0.6 --noise_type sym --type gce --beta 0.6
+python -u main.py --data_root /data --dataset cifar100  --lr 0.02 --lamb 8.0 -- tau 0.05 --r 0.6 --noise_type sym --type gce --beta 0.6
 
 ### sym noise ratio = 80%
-python -u main.py --data_root /data --dataset cifar100  --lr 0.02 --lamb 8.0 --ctr_thred 0.05 --r 0.8 --noise_type sym --type gce --beta 0.6
+python -u main.py --data_root /data --dataset cifar100  --lr 0.02 --lamb 8.0 -- tau 0.05 --r 0.8 --noise_type sym --type gce --beta 0.6
